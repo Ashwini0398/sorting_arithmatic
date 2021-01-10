0e0b77ba8decfd21d5ed514acf1f366d6586b2b4
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-10 17:12:34 +0530

    MODIFIED: store the result in dictionary

--- sortingComputation.sh
+++ sortingComputation.sh
@@ -10,3 +10,12 @@ val=$(($a+$b*$c))
 val2=$(($a*$b+$c))
 val3=$(($c+$a/$b))
 val4=$(($a%$b+$c))
+
+declare -A result
+
+result[computation1]=$val
+result[computation2]=$val2
+result[computation3]=$val3
+result[computation4]=$val4
+
+echo ${result[@]}
